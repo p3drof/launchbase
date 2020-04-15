cdd3548b39b3df098c3f0eaaefc7f67add3d5434
Pedro <pedrof.salim@gmail.com>
2020-04-15 00:48:27 -0300

    DESAFIO 1_3 FINALIZADO

--- desafio1_3.js
+++ desafio1_3.js
@@ -1,1 +1,9 @@
-teste
+const user = [
+    {name: "Carlos", techs: ["HTML", "CSS"]},
+    {name: "Jasmine", techs: ["JavaScript", "CSS"]},
+    {name: "Tuane", techs: ["HTML", "Node.js"]},
+]
+
+for(let i = 0; i < user.length; i++) {
+    console.log(`${user[i].name} trabalha com ${user[i].techs}`)
+}
