bf25d4fce09d3dcd36e07fe897d5ffb75ac1bc0d
Pedro <pedrof.salim@gmail.com>
2020-04-17 01:01:39 -0300

    Finalização

--- desafio1_3.js
+++ desafio1_3.js
@@ -2,8 +2,28 @@ const user = [
     {name: "Carlos", techs: ["HTML", "CSS"]},
     {name: "Jasmine", techs: ["JavaScript", "CSS"]},
     {name: "Tuane", techs: ["HTML", "Node.js"]},
+    {name: "Pedro", techs: ["HTML", "CSS", "Javascript", "C++", "Python"]},
+    {name: "Theo", techs: ["C++", "JAVA", "Ruby"]},
+    {name: "Sofia", techs: ["Photoshop", "HTML", "CSS"]}
 ]
 
-for(let i = 0; i < user.length; i++) {
+/*for(let i = 0; i < user.length; i++) {
     console.log(`${user[i].name} trabalha com ${user[i].techs}`)
+}*/
+
+function checaSeUsuarioUsaCSS(user) {
+    for(let i = 0; i < user.techs.length; i++) {
+        if(user.techs[i] == 'CSS') {
+            return true;
+        } 
+    } 
+    return false;
+}
+
+for(let i = 0; i < user.length; i++) {
+    const usuarioTrabalhaComCSS = checaSeUsuarioUsaCSS(user[i]);
+
+    if(usuarioTrabalhaComCSS) {
+        console.log(`O usuário  ${user[i].name} trabalha com CSS`)
+    }
 }
