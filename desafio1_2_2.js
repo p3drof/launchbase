23ccc55db4c7054b75f4b437de0c4f4143f87038
Pedro <pedrof.salim@gmail.com>
2020-04-23 03:24:19 -0300

    DADOS DO PROGRAMADOR

--- desafio1_2_2.js
+++ desafio1_2_2.js
@@ -7,5 +7,4 @@ const programmer = {
         {name: "Javascript", specialty: "Web/Mobile"},
     ],
 }
-
 console.log(`O usuário ${programmer.name} tem ${programmer.age} anos e usa a tecnologia ${programmer.techs[0].name} com especialidade em ${programmer.techs[0].specialty}`)
